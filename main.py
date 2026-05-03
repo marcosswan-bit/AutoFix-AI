1082ef86481f0d12d455c04ab55e148708633658
yoonchae <marcos.swan@ievcblanch.com>
2026-05-03 17:28:12 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from watcher import watch_directory
+
+if __name__ == "__main__":
+    print("🔍 AutoFix AI running...")
+    print("Watching ./project folder...\n")
+    watch_directory("./project")
